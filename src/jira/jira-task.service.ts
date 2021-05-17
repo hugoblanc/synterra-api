bb3a725cbc7041ecd5b288b2dfc7726cd05308f7
Hugo Blanc <hugoblanc.blend@gmail.com>
2021-05-17 16:42:07 +0200

    diable push

--- src/jira/jira-task.service.ts
+++ src/jira/jira-task.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { forkJoin } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
+import { OrderCreatedEvent } from '../event/zelty/order-created.event';
 import { IssueFactory } from './issue.factory';
 import { JiraHttpService } from './jira-http.service';
 import { IssueCreatedDto } from './models/jira-issue-created.dto';
-import { OrderCreatedEvent } from '../event/zelty/order-created.event';
 
 @Injectable()
 export class JiraTaskService {
@@ -32,18 +32,15 @@ export class JiraTaskService {
     console.log(issueFactory.getTask());
     console.log(issueFactory.getSubTasks());
     const mainTask = issueFactory.getTask();
-    this.jiraHttp
-      .post<IssueCreatedDto>('issue', mainTask)
-      .pipe(
-        mergeMap((issueCreatedDto: IssueCreatedDto) => {
-          issueFactory.addParentId(issueCreatedDto.id);
-          const subTasks = issueFactory.getSubTasks();
-          const postSubTasks$ = subTasks.map((sub) =>
-            this.jiraHttp.post('issue', sub),
-          );
-          return forkJoin(postSubTasks$);
-        }),
-      )
-      .subscribe();
+    this.jiraHttp.post<IssueCreatedDto>('issue', mainTask).pipe(
+      mergeMap((issueCreatedDto: IssueCreatedDto) => {
+        issueFactory.addParentId(issueCreatedDto.id);
+        const subTasks = issueFactory.getSubTasks();
+        const postSubTasks$ = subTasks.map((sub) =>
+          this.jiraHttp.post('issue', sub),
+        );
+        return forkJoin(postSubTasks$);
+      }),
+    );
   }
 }
