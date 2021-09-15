f843920ac0e1544b322d5b2cd5729a70134fc557
Hugo Blanc <hugoblanc.blend@gmail.com>
2021-09-15 19:25:16 +0200

    production ready

--- src/coordination/static-order-info.ts
+++ src/coordination/static-order-info.ts
@@ -52,107 +52,53 @@ export const JIRA_PROGRESS_COLUMN_ID = '3';
 
 // VARIABLE DATA
 
-// // TODO uncomment this
-// export const TAG_COMPOSANT = new Map<number, string>([
-//   [68231, '10006'], //BURGER BURGER
-//   [70806, '10006'], //Menu enfant aka cheeseburger
-//   [68232, '10009'],
-//   [68234, '10007'], // Accompagnement - Friteuse
-//   [68233, '10008'], // Salade
-//   [80177, '10008'], // Aperitivo
-//   // [68236, '4'],
-//   // [68237, '4'],
-// ]);
-// export const defaultComponent = { id: '10010' };
-// export const currentJiraProject = { id: '10001' };
-// export const COMPOSANT_CAPACITY = new Map<string, number>([
-//   ['10006', 8], //BURGER BURGER
-//   ['10007', 8], // Accompagnement - Friteuse
-//   ['10009', 1], // THAI
-//   ['10008', 1], // Salade
-//   ['10010', 30], // Others
-// ]);
-
-// export const COMPOSANT_DURATION_STATIC: AvgTimingDTO = {
-//   '10006': {
-//     '3': {
-//       count: 1,
-//       offset: 5 * 60 * 1000,
-//     },
-//   },
-//   '10007': {
-//     '3': {
-//       count: 1,
-//       offset: 4 * 60 * 1000,
-//     },
-//   },
-//   '10009': {
-//     '3': {
-//       count: 1,
-//       offset: 4 * 60 * 1000,
-//     },
-//   },
-//   '10008': {
-//     '3': {
-//       count: 1,
-//       offset: 4 * 60 * 1000,
-//     },
-//   },
-//   '10010': {
-//     '3': {
-//       count: 1,
-//       offset: 0 * 60 * 1000,
-//     },
-//   },
-// };
-
-// LOCAL DATA
-
+// TODO uncomment this
 export const TAG_COMPOSANT = new Map<number, string>([
-  [68231, '10013'], //BURGER BURGER
-  [70806, '10013'], //Menu enfant aka cheeseburger
-  [68234, '10014'], // Accompagnement - Friteuse
-  [68232, '10011'], // THAI
-  [68233, '10012'], // Salade
-  [80177, '10012'], // Aperitivo
+  [68231, '10006'], //BURGER BURGER
+  [70806, '10006'], //Menu enfant aka cheeseburger
+  [68232, '10009'],
+  [68234, '10007'], // Accompagnement - Friteuse
+  [68233, '10008'], // Salade
+  [80177, '10008'], // Aperitivo
   // [68236, '4'],
   // [68237, '4'],
 ]);
-
+export const defaultComponent = { id: '10010' };
+export const currentJiraProject = { id: '10001' };
 export const COMPOSANT_CAPACITY = new Map<string, number>([
-  ['10013', 8], //BURGER BURGER
-  ['10014', 8], // Accompagnement - Friteuse
-  ['10011', 1], // THAI
-  ['10012', 1], // Salade
-  ['10015', 30], // Others
+  ['10006', 8], //BURGER BURGER
+  ['10007', 8], // Accompagnement - Friteuse
+  ['10009', 1], // THAI
+  ['10008', 1], // Salade
+  ['10010', 30], // Others
 ]);
 
 export const COMPOSANT_DURATION_STATIC: AvgTimingDTO = {
-  '10013': {
+  '10006': {
     '3': {
       count: 1,
       offset: 5 * 60 * 1000,
     },
   },
-  '10014': {
+  '10007': {
     '3': {
       count: 1,
       offset: 4 * 60 * 1000,
     },
   },
-  '10011': {
+  '10009': {
     '3': {
       count: 1,
       offset: 4 * 60 * 1000,
     },
   },
-  '10012': {
+  '10008': {
     '3': {
       count: 1,
       offset: 4 * 60 * 1000,
     },
   },
-  '10015': {
+  '10010': {
     '3': {
       count: 1,
       offset: 0 * 60 * 1000,
@@ -160,5 +106,59 @@ export const COMPOSANT_DURATION_STATIC: AvgTimingDTO = {
   },
 };
 
-export const defaultComponent = { id: '10015' };
-export const currentJiraProject = { id: '10007' };
+// LOCAL DATA
+
+// export const TAG_COMPOSANT = new Map<number, string>([
+//   [68231, '10013'], //BURGER BURGER
+//   [70806, '10013'], //Menu enfant aka cheeseburger
+//   [68234, '10014'], // Accompagnement - Friteuse
+//   [68232, '10011'], // THAI
+//   [68233, '10012'], // Salade
+//   [80177, '10012'], // Aperitivo
+//   // [68236, '4'],
+//   // [68237, '4'],
+// ]);
+
+// export const COMPOSANT_CAPACITY = new Map<string, number>([
+//   ['10013', 8], //BURGER BURGER
+//   ['10014', 8], // Accompagnement - Friteuse
+//   ['10011', 1], // THAI
+//   ['10012', 1], // Salade
+//   ['10015', 30], // Others
+// ]);
+
+// export const COMPOSANT_DURATION_STATIC: AvgTimingDTO = {
+//   '10013': {
+//     '3': {
+//       count: 1,
+//       offset: 5 * 60 * 1000,
+//     },
+//   },
+//   '10014': {
+//     '3': {
+//       count: 1,
+//       offset: 4 * 60 * 1000,
+//     },
+//   },
+//   '10011': {
+//     '3': {
+//       count: 1,
+//       offset: 4 * 60 * 1000,
+//     },
+//   },
+//   '10012': {
+//     '3': {
+//       count: 1,
+//       offset: 4 * 60 * 1000,
+//     },
+//   },
+//   '10015': {
+//     '3': {
+//       count: 1,
+//       offset: 0 * 60 * 1000,
+//     },
+//   },
+// };
+
+// export const defaultComponent = { id: '10015' };
+// export const currentJiraProject = { id: '10007' };
